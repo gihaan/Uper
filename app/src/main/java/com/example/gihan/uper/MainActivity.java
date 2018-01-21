96fdd2e0e80c480fe035032c20dfe6c965b30c93
Gihan <gemy6987@gmail.com>
2018-01-21 06:49:02 -0800

    my commit message

---
+++ app/src/main/java/com/example/gihan/uper/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.gihan.uper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
